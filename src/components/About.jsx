7cf691d767a66e098d64b2ae4194580d5a1ec0e6
Alejo Acevedo <alejo.acevedo@wolox.com.ar>
2019-07-30 12:39:18 -0300

    Style Components S03E01

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,31 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
 import Social from './Social';
 
-const About = ({ avatar, name, profession, bio, address, social}) =>(
-    <div className="About">
-        <div className="About-container">
-            <div className="About-avatar">
-                <figure>
-                    <img src={avatar} alt={name}/>
-                </figure>
-            </div>
-            <div className="About-name">
-                <h2>{name}</h2>
-            </div>
-            <div className="About-profession">
-                <p>{profession}</p>
-            </div>
-            <div className="About-desc">
-                <p>{bio}</p>
-            </div>
-            <div className="About-location">
-                <p>{address}</p>
-            </div>
-            <div className="About-social">
-                <Social social={social}> </Social>
-            </div>
-        </div>
+const AboutStyle = styled.div`
+  text-align: center;
+`;
+
+const AboutAvatar = styled.div`
+  padding: 2em 0 0 0;
+`;
+
+const AboutImg = styled.img`
+  border-radius: 100%;
+  width: 160px;
+  height: 160px;
+  border: 2px solid;
+  margin: 0 auto;
+  display: block;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.6);
+`;
+
+const About = ({ avatar, name, profession, bio, address, social }) => (
+  <AboutStyle>
+    <div className="About-container">
+      <AboutAvatar>
+        <figure>
+          <AboutImg src={avatar} alt={name} />
+        </figure>
+      </AboutAvatar>
+      <div className="About-name">
+        <h2>{name}</h2>
+      </div>
+      <div className="About-profession">
+        <p>{profession}</p>
+      </div>
+      <div className="About-desc">
+        <p>{bio}</p>
+      </div>
+      <div className="About-location">
+        <p>{address}</p>
+      </div>
+      <div className="About-social">
+        <Social social={social}> </Social>
+      </div>
     </div>
+  </AboutStyle>
 );
 
 export default About;
